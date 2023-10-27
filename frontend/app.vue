11f82f2551c02c2182f3e1f76ae7acad72944d0f
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2023-10-27 14:29:34 +0530

    Reset UI and refactor approach

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,218 +1,104 @@
 <template>
   <div class="container">
-    <!-- Shotclock Timer -->
-    <div class="timer">
-      <span class="countdown">{{ shotClockTimerRemaining }}s</span>
+    <div class="section status-monitor">
+      <span class="section-title">STATUS MONITOR</span>
+      <div class="monitor-card"></div>
     </div>
-
-     <!-- Timer Controls -->
-     <div class="controls">
-      <button class="control-btn start" @click="handleShotClock">{{ shotClockLabel }}</button>
-      <button class="control-btn reset" @click="resetShotClock">Reset</button>
+    <div class="section">
+      <span class="section-title">POINTS</span>
+      <div>
+        <button class="btn">1 Point</button>
+        <button class="btn">2 Points</button>
+      </div>
     </div>
-
-    <!-- Score Buttons -->
-    <div class="score-buttons">
-      <button class="score-btn" @click="scoreAndPause(1)">1 Point</button>
-      <button class="score-btn" @click="scoreAndPause(2)">2 Points</button>
-      <button class="score-btn" @click="scoreAndPause(3)">3 Points</button>
+    <div class="section">
+      <span class="section-title">POWERPLAY</span>
+      <div>
+        <label>
+          <input type="radio" name="powerplay" value="1" v-model="powerplayChoice" /> 1
+        </label>
+        <label>
+          <input type="radio" name="powerplay" value="2" v-model="powerplayChoice" /> 2
+        </label>
+      </div>
+    </div>
+    <div class="section">
+      <span class="section-title">GAMEPLAY</span>
+      <div>
+        <button class="btn">BUZZER</button>
+        <button class="btn">HALFTIME</button>
+      </div>
+    </div>
+    <div class="section">
+      <span class="section-title">CELEBRATIONS</span>
+      <div>
+        <button class="btn">Effect 1</button>
+        <button class="btn">Effect 2</button>
+        <button class="btn">Effect 3</button>
+        <button class="btn">Effect 4</button>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue';
-
-// Reactive references
-const activeTimer = ref(false);
-const shotClockTimerRemaining = ref(14); // seconds
-const interval = ref(null); // use a single instance of interval
-const shotClockLabel = ref('Start');
-
-// Handle the shotclock on first button press
-const handleShotClock = () => {
-  if (!activeTimer.value && shotClockTimerRemaining.value === 14) {
-    startTimer();
-    shotClockLabel.value = 'Pause';
-  } else if (activeTimer.value) {
-    activeTimer.value = false;
-    shotClockLabel.value = 'Resume';
-    clearInterval(interval.value); // Clear the interval when pausing
-  } else {
-    startTimer();
-    shotClockLabel.value = 'Pause';
-  }
-};
-
-const pauseTimer = () => {
-  activeTimer.value = false;
-  shotClockLabel.value = 'Resume';
-  clearInterval(interval.value);
-};
-
-const resetShotClock = () => {
-  pauseTimer();
-  shotClockTimerRemaining.value = 14;
-  shotClockLabel.value = 'Start';
-};
-
-const scoreAndPause = async (payload) => {
-  pauseTimer();
-  await sendRequest(`api/dummy${payload}`);
-};
-
-// Start the countdown timer
-const startTimer = () => {
-  if (interval.value) {
-    clearInterval(interval.value); // Clear any existing interval
-  }
-  
-  activeTimer.value = true;
-  const countdown = () => {
-    if (shotClockTimerRemaining.value > 0 && activeTimer.value) {
-      shotClockTimerRemaining.value--;
-    } else if (shotClockTimerRemaining.value <= 0) {
-      clearInterval(interval.value);
-      sendRequest('api/dummyend');
-      resetTimer();
-    }
-  };
-  
-  interval.value = setInterval(countdown, 1000);
-};
-
-// Reset the timer
-const resetTimer = () => {
-  activeTimer.value = false;
-  shotClockTimerRemaining.value = 14;
-  shotClockLabel.value = 'Start';
-};
-
-// Function to send API requests using fetch
-const sendRequest = async (apiCommand) => {
-  try {
-    // replace with your actual server URL
-    const url = `http://${IP}/json/${apiCommand}`;
-    const response = await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-      // TODO: add body later
-    });
-
-    // check if the request was successful
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json(); // we are working with WLED json API
-    console.log(data);
-    // handle response or update state as needed
-  } catch (error) {
-    console.error('An error occurred:', error);
-  }
+<script>
+export default {
+  data() {
+    return {
+      powerplayChoice: '1',
+    };
+  },
 };
 </script>
 
 <style scoped>
-:root {
-  --btn-start-color: #e76f51;
-  --btn-point1-color: #f4a261;
-  --btn-point2-color: #775253;
-  --btn-point3-color: #2a9d8f;
-  --btn-hover-glow: rgba(255, 255, 255, 0.3);
-}
-
 .container {
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  height: 100vh;
-  font-family: 'Arial', sans-serif;
+  background-color: white;
+  padding: 20px;
 }
 
-/* Shotclock Timer */
-.timer {
-  margin-bottom: 2rem;
-  background-color: #2F3E50;
-  border-radius: 10px;
-  padding: 1rem 2rem;
-}
-
-.countdown {
-  font-size: 3rem;
-  color: #FFFFFF;
-}
-
-/* Timer Controls */
-.controls {
+.section {
   display: flex;
-  justify-content: center;
-  gap: 1rem;
-  margin-bottom: 2rem;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 20px;
+  border-bottom: 1px dashed lightgrey;
+  padding-bottom: 20px;
 }
 
-.control-btn {
-  padding: 0.8rem 1.5rem;
-  font-size: 1rem;
+.section-title {
+  font-weight: bold;
+  flex: 1;
+}
+
+.monitor-card {
+  width: 80%;
+  height: 100px;
+  background-color: #f4f4f4;
+  border: 1px solid #ddd;
+}
+
+.btn {
+  margin: 5px;
+  padding: 10px 20px;
   border: none;
-  border-radius: 5px;
   cursor: pointer;
-  transition: all 0.3s ease;
+  transition: background-color 0.3s ease;
 }
 
-.control-btn.start {
-  background-color: #E63946;
-  color: #FFFFFF;
+.btn:hover {
+  background-color: #f0f0f0;
 }
 
-.control-btn.reset {
-  background-color: #F1FAEE;
-  color: #1D3557;
-}
-
-.control-btn:hover {
-  transform: scale(1.05);
-}
-
-/* Score Buttons */
-.score-buttons {
-  display: flex;
-  justify-content: center;
-  gap: 1rem;
-}
-
-.score-btn {
-  padding: 1rem 2rem;
-  font-size: 1.1rem;
-  border: none;
-  border-radius: 10px;
-  cursor: pointer;
-  transition: all 0.3s ease;
-  color: #FFFFFF;
-}
-
-.score-btn:nth-child(1) {
-  background-color: #FF9B54;
-}
-
-.score-btn:nth-child(2) {
-  background-color: #8E5572;
-}
-
-.score-btn:nth-child(3) {
-  background-color: #3A9D8F;
-}
-
-.score-btn:hover {
-  transform: translateY(-5px);
-}
-
-@media (max-width: 480px) {
-  .score-buttons {
-    flex-direction: column;
+/* Adjustments for responsiveness can be added below */
+@media (max-width: 768px) {
+  .btn {
+    margin: 3px;
+    padding: 8px 16px;
   }
 }
 </style>
