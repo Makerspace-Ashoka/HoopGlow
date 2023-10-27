17e06d9a1225e28fb68b66da90832c9c9a69e879
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2023-10-27 17:14:05 +0530

    Add basic button handlers and API calls

--- frontend/app.vue
+++ frontend/app.vue
@@ -2,14 +2,16 @@
   <div class="container">
     <div class="section status-monitor">
       <span class="section-title" id="status-monitor">Status Monitor</span>
-      <div class="monitor-card"></div>
+      <div class="monitor-card">
+        <div class="monitor-text">{{ timer }}</div>
+      </div>
     </div>
     <div class="section">
       <span class="section-title">Points</span>
       <div class="section-body">
-        <button class="btn" style="background-color: #5D2A42;  color: #fbf5f3;">1 Point</button>
-        <button class="btn" style="background-color: #3a9d8f;  color: #fbf5f3;">2 Points</button>
-        <button class="btn" style="background-color: #D9F9A5;">3 Points</button>
+        <button class="btn" style="background-color: #5D2A42;  color: #fbf5f3;" @click="handleButton('1Point')">1 Point</button>
+        <button class="btn" style="background-color: #3a9d8f;  color: #fbf5f3;" @click="handleButton('2Point')">2 Points</button>
+        <button class="btn" style="background-color: #D9F9A5;" @click="handleButton('3Point')">3 Points</button>
       </div>
     </div>
     <div class="section">
@@ -25,29 +27,116 @@
     <div class="section">
       <span class="section-title">Gameplay</span>
       <div class="section-body">
-        <button class="btn" style="background-color: #E76F51; color: #fbf5f3;">Buzzer</button>
+        <button class="btn" style="background-color: #E76F51; color: #fbf5f3;" @click="handleButton('buzzer')">Buzzer</button>
       </div>
     </div>
     <div class="section">
       <span class="section-title">Celebrations</span>
       <div class="section-body">
-        <button class="btn" style="background-color: #FFD639;">Effect 1</button>
-        <button class="btn" style="background-color: #FFD639;">Effect 2</button>
-        <button class="btn" style="background-color: #FFD639;">Effect 3</button>
-        <button class="btn" style="background-color: #FFD639;">Effect 4</button>
+        <button class="btn" style="background-color: #FFD639;" @click="handleButton('1Effect')">Effect 1</button>
+        <button class="btn" style="background-color: #FFD639;" @click="handleButton('2Effect')">Effect 2</button>
+        <button class="btn" style="background-color: #FFD639;" @click="handleButton('3Effect')">Effect 3</button>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      powerplayChoice: '1',
-    };
-  },
+<script setup>
+import { ref, watch } from 'vue';
+
+const powerplayType = ref('1');
+const timer = ref(60);
+const isPowerplayRunning = ref(false);
+
+const handleButton = async (buttonType) => {
+  let payload = {};
+  switch(buttonType) {
+    case '1Point':
+      // We will never come here in powerplay
+      payload = {"playlist":{"ps":[1,2,4,3],"dur":[10,20,5,100],"transition":[7,7,7,7],"repeat":0,"r":false,"end":0},"on":true};
+      break;
+    case '2Point':
+      payload = {};
+      if(isPowerplayRunning.value) {
+        payload = {"playlist":{"ps":[7,6,4,3],"dur":[10,20,5,100],"transition":[7,7,7,7],"repeat":0,"r":false,"end":0},"on":true};
+      } else {
+        // TODO: if powerplay is running then set segment colour after wipe
+        payload = {};
+      }
+      break;
+    case '3Point':
+      // We only come here in powerplay
+      payload = {"playlist":{"ps":[10,9,4,3],"dur":[10,20,10,100],"transition":[7,7,7,7],"repeat":0,"r":false,"end":0},"on":true};
+      break;
+    case 'buzzer':
+      payload = {"playlist":{"ps":[12,13],"dur":[130,70],"transition":[7,7],"repeat":0,"r":false,"end":0},"on":true};
+      break;
+    case '1Effect':
+      payload = {"on":true,"bri":197,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":60,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"col":[[99,255,250],[191,255,54],[175,255,173]],"fx":60,"sx":220,"ix":255,"pal":0,"sel":true,"rev":false,"mi":false},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]};
+      break;
+    case '2Effect':
+      payload = {"on":true,"bri":197,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":60,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"col":[[99,255,250],[191,255,54],[175,255,173]],"fx":60,"sx":220,"ix":255,"pal":0,"sel":true,"rev":false,"mi":false},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]};
+      break;
+    case '3Effect':
+      payload = {"on":true,"bri":197,"transition":7,"mainseg":0,"seg":[{"id":0,"start":0,"stop":60,"grp":1,"spc":0,"of":0,"on":true,"frz":false,"bri":255,"cct":127,"col":[[99,255,250],[191,255,54],[175,255,173]],"fx":60,"sx":220,"ix":255,"pal":0,"sel":true,"rev":false,"mi":false},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0},{"stop":0}]};
+      break;
+    // ... add cases for other buttons ...
+  }
+  // Send API call with the payload
+  await sendPOSTRequest("state", payload);
+  // Example: fetch('API_ENDPOINT', { method: 'POST', body: JSON.stringify(payload) });
 };
+
+const startPowerplay = () => {
+  // Send API call on start keypress
+  // Example: fetch('API_ENDPOINT_START', { method: 'POST', body: JSON.stringify({ powerplayType: powerplayType.value }) });
+
+  isPowerplayRunning.value = true;
+  const intervalId = setInterval(() => {
+    if (timer.value > 0) {
+      timer.value--;
+    } else {
+      clearInterval(intervalId);
+      isPowerplayRunning.value = false;
+      timer.value = 60;  // Reset timer
+      // Send another API call when timer runs out
+      // Example: fetch('API_ENDPOINT_END', { method: 'POST', body: JSON.stringify({ powerplayType: powerplayType.value }) });
+    }
+  }, 1000);
+};
+
+// Function to send API requests using fetch
+const sendPOSTRequest = async (apiCommand, payload) => {
+  const IP = "192.168.0.1";
+  try {
+    // replace with your actual server URL
+    const url = `http://${IP}/json/${apiCommand}`;
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: payload
+    });
+
+    // check if the request was successful
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+};
+
+watch(timer, (newVal) => {
+  if (newVal === 0) {
+    isPowerplayRunning.value = false;
+  }
+});
+
 </script>
 
 <style scoped>
