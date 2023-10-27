ca7af9e086ab9530527938a4db9acafedf8ccedd
Deepraj Pandey <15888623+DeeprajPandey@users.noreply.github.com>
2023-10-27 15:31:02 +0530

    Add google-fonts module, update title text typeface, and add button colours

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/google-fonts',
+  ],
+  googleFonts: {
+    families: {
+      Nunito: true
+    }
+  },
   devtools: { enabled: true },
   css: [
     'assets/css/main.css',
-  ],
+  ]
 })
